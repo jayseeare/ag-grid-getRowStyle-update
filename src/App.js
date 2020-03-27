4a7445f69de9c6d087b6477ff3b66b030b3fac70
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-03-27 22:23:07 +0000

    Reproduce error in example grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
-function App() {
+const makes = [
+  "Toyota",
+  "Ford",
+  "Porsche"
+];
+
+const useRotatingStyle = (name, color) => {
+  const [makeToHighlightIndex, setMakeToHighlightIndex] = useState(0);
+
+  const getStyle = useCallback(({ data }) => {
+    console.log(name, "getStyle", makeToHighlightIndex, makes[makeToHighlightIndex]);
+    return data.make === makes[makeToHighlightIndex]
+      ? { backgroundColor: color }
+      : { backgroundColor: "rgba(0,0,0,0)" }
+  }, [makeToHighlightIndex]);
+
+  const rotate = () => {
+    const nextMakeIndex = (makeToHighlightIndex + 1) % makes.length
+    console.log(name, "rotate", nextMakeIndex, makes[nextMakeIndex]);
+    setMakeToHighlightIndex(nextMakeIndex);
+  };
+
+  return { getStyle, rotate };
+};
+
+const App = () => {  
+  const { getStyle: getCellStyle, rotate: rotateCellStyle } = useRotatingStyle("Cell", "#F2BD99");
+
+  const columnDefs = [
+    { headerName: "Make", field: "make", cellStyle: getCellStyle },
+    { headerName: "Model", field: "model" },
+    { headerName: "Price", field: "price" }
+  ];
+  
+  const rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 }, 
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 }
+  ];
+
+  const { getStyle: getRowStyle, rotate: rotateRowStyle } = useRotatingStyle("Row", "#99CEF2");
+
+  const [gridApi, setGridApi] = useState();
+  const onGridReady = useCallback(({ api }) => {
+    setGridApi(api);
+  }, []);
+
+  useEffect(() => {
+    if (gridApi) {
+      console.log("redraw");
+      gridApi.redrawRows();
+    }
+  }, [gridApi, getRowStyle, getCellStyle]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="grid-wrapper ag-theme-balham"
+    >
+      <button onClick={rotateRowStyle}>Rotate Row Style</button>
+      <button onClick={rotateCellStyle}>Rotate Cell Style</button>
+      <AgGridReact
+        getRowStyle={getRowStyle}
+        columnDefs={columnDefs}
+        rowData={rowData}
+        onGridReady={onGridReady}
+      />
     </div>
   );
-}
+};
 
 export default App;
